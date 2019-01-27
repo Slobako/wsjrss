eb331a1730879527be1f4837cd41175d15b9e4db
Slobodan <slobodan@Vladans-MacBook-Pro.local>
2019-01-27 14:40:45 -0500

    Set launch screen

---
+++ wsjrss/wsjrssTests/wsjrssTests.swift
@@ -0,0 +1,34 @@
+//
+//  wsjrssTests.swift
+//  wsjrssTests
+//
+//  Created by Slobodan on 1/27/19.
+//  Copyright © 2019 Slobodan. All rights reserved.
+//
+
+import XCTest
+@testable import wsjrss
+
+class wsjrssTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
